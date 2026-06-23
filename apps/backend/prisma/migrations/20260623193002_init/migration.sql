f98e25e9d47b775982e8b77b4daf2f96884cd89f
RafaelMedeirosDev <oliver.rafadev@gmail.com>
2026-06-23 16:32:11 -0300

    feat: adiciona model Contact e migration inicial

---
+++ apps/backend/prisma/migrations/20260623193002_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "contact" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "contact_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "contact_phone_key" ON "contact"("phone");
